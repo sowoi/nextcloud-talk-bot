c082ffdefe927da8c890caf6f778540ab3c36da3
Okko <okko@fahmed.de>
2023-04-06 23:44:22 +0200

    headers

---
+++ nextcloud_talk_bot/headers.py
@@ -0,0 +1,18 @@
+def create_headers(password):
+    """
+    Helper script.
+    Create and return headers for Nextcloud API requests.
+
+    This function generates a dictionary containing headers required for making
+    requests to the Nextcloud API. It sets the 'Authorization' header with the
+    provided password.
+    """
+
+    headers = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'OCS-APIRequest': 'true',
+        'Authorization': f"Bearer {password}",
+        'Accept-Language': 'en'
+    }
+    return headers
