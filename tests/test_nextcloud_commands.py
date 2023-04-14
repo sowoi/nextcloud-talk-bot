f44c79f908788f858a4fd6befcadbff96a43a7b1
Okko <10256263+sowoi@users.noreply.github.com>
2023-04-14 17:05:07 +0200

    commands unittest

--- tests/test_nextcloud_commands.py
+++ tests/test_nextcloud_commands.py
@@ -2,30 +2,26 @@ import unittest
 from unittest.mock import MagicMock, patch
 from nextcloud_talk_bot.nextcloud_commands import NextcloudCommands
 
-class MockBot:
-    def __init__(self):
-        self.NEXTCLOUD_URL = "https://example.com/nextcloud"
-
-class TestNextcloudCommandsBot(NextcloudCommands):
-    def __init__(self, module_name, bot):
-        super().__init__(module_name)
-        self.bot = bot
 
 class TestNextcloudCommands(unittest.TestCase):
+    with patch('nextcloud_talk_bot.Nextcloudtalkbot') as mock_bot:
+        mock_bot.NEXTCLOUD_URL = 'https://example.com'
+        mock_bot.USERNAME = 'user'
+        mock_bot.PASSWORD = 'password'
 
     def test_map_input_to_module(self):
-        nc_command = TestNextcloudCommandsBot("activities", bot=MockBot())
+        nc_command = NextcloudCommands("activities")
         self.assertEqual(nc_command.module_name, "nextcloud_activities")
 
     @patch('inspect.getmembers')
     def test_get_first_class(self, mock_getmembers):
-        nc_command = TestNextcloudCommandsBot("activities", bot=MockBot())
+        nc_command = NextcloudCommands("activities")
         nc_command.module = MagicMock()
         nc_command.get_first_class()
         mock_getmembers.assert_called_once()
 
     def test_print_first_class_docstring(self):
-        nc_command = TestNextcloudCommandsBot("activities", bot=MockBot())
+        nc_command = NextcloudCommands("activities")
         nc_command.get_first_class = MagicMock(
             return_value=MagicMock(
                 __name__="TestClass",
@@ -33,7 +29,7 @@ class TestNextcloudCommands(unittest.TestCase):
         nc_command.print_first_class_docstring()
 
     def test_print_method_docstring(self):
-        nc_command = TestNextcloudCommandsBot("activities", bot=MockBot())
+        nc_command = NextcloudCommands("activities")
         nc_command.get_first_class = MagicMock(
             return_value=MagicMock(
                 test_method=MagicMock(
@@ -42,7 +38,7 @@ class TestNextcloudCommands(unittest.TestCase):
 
 
 def test_call_class_method(self):
-    nc_command = TestNextcloudCommandsBot("activities", bot=MockBot())
+    nc_command = NextcloudCommands("activities")
     nc_command.get_first_class = MagicMock(return_value=MagicMock())
     mock_method = MagicMock(return_value="Test result")
     nc_command.bot = MagicMock(activities=MagicMock(test_method=mock_method))
@@ -52,7 +48,7 @@ def test_call_class_method(self):
 
     @patch('inspect.getmembers')
     def test_print_available_classes_and_methods(self, mock_getmembers):
-        nc_command = TestNextcloudCommandsBot("activities", bot=MockBot())
+        nc_command = NextcloudCommands("activities")
         nc_command.module = MagicMock()
         nc_command.print_available_classes_and_methods()
         mock_getmembers.assert_called_once()
